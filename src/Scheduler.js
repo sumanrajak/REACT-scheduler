d97d1e90bfb9938933e9c21eaadd72ea68b9f078
Sumanrajak <sumanrajak2001@gmail.com>
2020-12-23 22:50:38 +0530

    icon set

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -10,8 +10,13 @@ import Tab from 'react-bootstrap/Tab'
 import Tabs from 'react-bootstrap/Tabs'
 import Form from 'react-bootstrap/Form'
 import axios from 'axios'
-
-
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import NoteAddOutlinedIcon from '@material-ui/icons/NoteAddOutlined';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PeopleIcon from '@material-ui/icons/People';
+import PersonIcon from '@material-ui/icons/Person';
+import BusinessSharpIcon from '@material-ui/icons/BusinessSharp';
 const Scheduler = () => {
     //data
 
@@ -135,7 +140,7 @@ const  Eventpost_internal = async(e)=>{
        <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
      </td></tr> */}
      <tr>
-         <td colSpan={10}> 
+         <td colSpan={5}> 
          <Tabs defaultActiveKey="home"  transition={false} id="noanim-tab-example" style={{    width: "fit-content"}}>
 
 
@@ -150,29 +155,35 @@ const  Eventpost_internal = async(e)=>{
           <tbody>
           <tr>
               <td colSpan={4}>
-              <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
+              <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.EndTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
               </td>
          </tr>
-         <tr><td className="e-textlabel">company name </td><td colSpan={4}>
+         <tr ><td className="e-textlabel"><BusinessSharpIcon/> </td><td colSpan={4}>
          <DropDownListComponent id="company_name" placeholder='company name' data-name="company_name" className="e-field" style={{ width: '100%' }} dataSource={['abc', 'xuz', 'sds']} value={props.company_name || null}></DropDownListComponent>
 </td></tr>
-<tr><td className="e-textlabel">contact person </td><td colSpan={4}>  <DropDownListComponent id="contact_persons" placeholder='contact person' data-name="contact_persons" className="e-field" style={{ width: '100%' }} dataSource={['Neasssw', 'Requsdsdested', 'Cosddsnfirmed']} value={props.contact_persons || null}></DropDownListComponent>
+<tr><td className="e-textlabel"><PeopleIcon/></td><td colSpan={4}>  <DropDownListComponent id="contact_persons" placeholder='contact person' data-name="contact_persons" className="e-field" style={{ width: '100%' }} dataSource={['Neasssw', 'Requsdsdested', 'Cosddsnfirmed']} value={props.contact_persons || null}></DropDownListComponent>
 </td></tr>
-<tr><td>from</td><td colSpan={4}>  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
-</td></tr>
-<tr><td className="e-textlabel">To</td><td colSpan={4}>
-       <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="EndTime" data-name="EndTime" value={new Date(props.endTime || props.EndTime)} className="e-field"></DateTimePickerComponent>
-     </td></tr><tr><td className="e-textlabel">meetings</td><td colSpan={4}>
+<tr>
+  <td><AccessTimeIcon/></td> 
+  <td><DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime"style={{ width: '70%',paddingLeft:"5px"  }}  value={new Date(props.StartTime || props.StartTime_)} className="e-field"></DateTimePickerComponent>
+  </td> 
+  <td>to</td>
+  <td >
+  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="endtime" data-name="endtime" style={{ width: '70%',  }}  value={new Date(props.endtime || props.endtime)} className="e-field"></DateTimePickerComponent>
+      </td>
+</tr>
+<tr>
+<td className="e-textlabel">meetings</td><td colSpan={4}>
      <DropDownListComponent id="cliant_meeting" placeholder='meeting' text="true" data-name="cliant_meeting" className="e-field" style={{ width: '100%' }} dataSource={['meeting', 'email', 'call']} value={props.cliant_meeting || null}></DropDownListComponent>
      </td></tr>
-     <tr><td className="e-textlabel">cr associates </td><td colSpan={4}>
+     <tr><td className="e-textlabel"><AccountCircleIcon/> </td><td colSpan={4}>
      <DropDownListComponent id="cr_associates" placeholder='cr associates' data-name="cr_associates" className="e-field" style={{ width: '100%' }} dataSource={['cr associates1', 'cr associates2', 'cr associates3']} value={props.cr_associates || null}></DropDownListComponent>
      </td></tr>
-     <tr><td className="e-textlabel">reference</td><td colSpan={4}>
+     <tr><td className="e-textlabel"><PersonIcon/></td><td colSpan={4}>
      <textarea id="reference" className="e-field e-input" name="reference" rows={3} cols={50} style={{ width: '100%', height: '20px !important', resize: 'vertical' }} value={props.reference || null}></textarea>
      </td></tr>
      
-     <tr><td className="e-textlabel">notes</td><td colSpan={4}>
+     <tr><td className="e-textlabel"><NoteAddOutlinedIcon/></td><td colSpan={4}>
        <textarea id="notes_cliant" className="e-field e-input" name="notes" rows={3} cols={50} style={{ width: '100%', height: '40px !important', resize: 'vertical' }} value={props.notes_cliant || null}></textarea>
      </td></tr>
 
@@ -187,25 +198,33 @@ const  Eventpost_internal = async(e)=>{
 
 
   <Tab eventKey="travel" title="travel">
-  <table>
+  <table >
           <tbody>
           
-         <tr><td>from</td> <td><DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime_trav" data-name="StartTime_trav" value={new Date(props.startTime_trav || props.startTime_trav)} className="e-field"></DateTimePickerComponent>
+<tr style={{ paddingLeft: "4em"}}>
+  <td><AccessTimeIcon/></td > 
+  <td colSpan={4} ><DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime_trav" data-name="StartTime_trav"style={{ width: '70%',paddingLeft:"5px"  }}  value={new Date(props.startTime_trav || props.startTime_trav)} className="e-field"></DateTimePickerComponent>
+  </td> 
+  <td>to</td>
+  <td >
+  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="endtime_trav" data-name="endtime_trav" style={{ width: '70%',  }}  value={new Date(props.endtime_trav || props.endtime_trav)} className="e-field"></DateTimePickerComponent>
+      </td>
+</tr>
+<tr>
+  <td><LocationOnIcon/></td> 
+  <td><textarea id="travel_from" className="e-field e-input" name="travel_from" cols={50} style={{ width: '70%', paddingLeft:"5px"  }}  value={props.travel_from || null}></textarea>
 
-</td></tr>
-<tr><td className="e-textlabel">To</td><td colSpan={4}>
-<DateTimePickerComponent format='dd/MM/yy hh:mm a' id="endtime_trav" data-name="endtime_trav" value={new Date(props.endtime_trav || props.endtime_trav)} className="e-field"></DateTimePickerComponent>
-     </td></tr>
-         <tr><td>from</td> <td><textarea id="travel_from" className="e-field e-input" name="travel_from" cols={50} style={{ width: '100%', height: '10px !important', resize: 'vertical' }}  value={props.travel_from || null}></textarea>
+</td>
+<td>to</td>
+<td colSpan={4}>
+<textarea id="travel_to" className="e-field e-input" name="travel_to" cols={50} style={{ width: '70%', }}  value={props.travel_to || null}></textarea>
+     </td>
+</tr>
 
-</td></tr>
-<tr><td className="e-textlabel">To</td><td colSpan={4}>
-<textarea id="travel_to" className="e-field e-input" name="travel_to" cols={50} style={{ width: '100%', height: '5px !important', resize: 'vertical' }}  value={props.travel_to || null}></textarea>
-     </td></tr>
-     <tr><td className="e-textlabel">notes</td><td colSpan={4}>
+     <tr><td className="e-textlabel"><NoteAddOutlinedIcon/></td><td colSpan={4}>
        <textarea id="notes_travel" className="e-field e-input" name="notes_travel_descscription" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }}  value={props.notes_travel || null}></textarea>
      </td></tr>
-     <tr><td className="e-textlabel">acompanied by</td><td colSpan={4}>
+     <tr><td className="e-textlabel"><AccountCircleIcon/></td><td colSpan={4}>
        <textarea id="travel_acompanied" className="e-field e-input" name="travel_acompanied" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }}  value={props.travel_acompanied || null}></textarea>
      </td></tr>
 
@@ -220,14 +239,18 @@ const  Eventpost_internal = async(e)=>{
   <Tab eventKey="internal" title="internal">
   <table>
           <tbody>
-          <tr>
-              <td colSpan={4}>
-              <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime" data-name="StartTime" value={new Date(props.startTime || props.StartTime)} className="e-field"></DateTimePickerComponent>
-              </td>
-         </tr>
+          <tr style={{ paddingLeft: "4em"}}>
+  <td><AccessTimeIcon/></td> 
+  <td ><DateTimePickerComponent format='dd/MM/yy hh:mm a' id="StartTime_trav" data-name="StartTime_trav"style={{ width: '70%',paddingLeft:"5px"  }}  value={new Date(props.startTime_trav || props.startTime_trav)} className="e-field"></DateTimePickerComponent>
+  </td> 
+  <td>to</td>
+  <td >
+  <DateTimePickerComponent format='dd/MM/yy hh:mm a' id="endtime_trav" data-name="endtime_trav" style={{ width: '70%',  }}  value={new Date(props.endtime_trav || props.endtime_trav)} className="e-field"></DateTimePickerComponent>
+      </td>
+</tr>
         
-     <tr><td className="e-textlabel">notes</td><td colSpan={4}>
-       <textarea id="internal_notes" className="e-field e-input" name="internal_notes" rows={3} cols={50} style={{ width: '100%', height: '60px !important', resize: 'vertical' }} value={props.internal_notes || null}></textarea>
+     <tr><td className="e-textlabel"><NoteAddOutlinedIcon/></td><td colSpan={4}>
+       <textarea id="internal_notes" className="e-field e-input" name="internal_notes" rows={3} cols={50} style={{ width: '100%', height: '60px !important',  }} value={props.internal_notes || null}></textarea>
      </td></tr>
      <tr><td className="e-textlabel">meetings</td><td colSpan={4}>
      <DropDownListComponent id="internal_meeting" placeholder='company name' data-name="internal_meeting" className="e-field" style={{ width: '100%' }} dataSource={['meeting', 'email', 'call']} value={props.internal_meeting || null}></DropDownListComponent>
@@ -241,7 +264,7 @@ const  Eventpost_internal = async(e)=>{
          </td>
      </tr>
      
-    </tbody></table> : <div></div>);}
+    </tbody></table> : <div>lol</div>);}
 
 
 const onEventRendered=(args)=> {
@@ -264,8 +287,8 @@ const onEventRendered=(args)=> {
 
 const onActionBegin=(args)=> { 
     if (args.requestType === 'eventCreate') { 
-      
       const eventData = args.data[0];
+//
       if(eventData.EventType=="TRAVEL"){
         Eventpost_travel(eventData);
       }else if(eventData.EventType=="CLIANT"){
